4f56b658da2dfb13e9923c7781a9bcfe36593909
b178527 <1157864699@qq.com>
2026-04-13 15:35:39 +0800

    Site updated: 2026-04-13 15:35:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["651cb7fa30c0/","1c4a5286cf6d/","728d0100c79c/","5ac32811adb9/"];function toRandomPost(){
+var posts=["728d0100c79c/","5ac32811adb9/","1c4a5286cf6d/","651cb7fa30c0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
