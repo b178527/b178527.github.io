16d92ca1faa6b9e418a22a96700ee827381336f3
b178527 <1157864699@qq.com>
2026-05-14 16:41:34 +0800

    Site updated: 2026-05-14 16:41:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["728d0100c79c/","651cb7fa30c0/","5ac32811adb9/","1c4a5286cf6d/","f03a7354ba49/"];function toRandomPost(){
+var posts=["f03a7354ba49/","5ac32811adb9/","651cb7fa30c0/","1c4a5286cf6d/","728d0100c79c/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
