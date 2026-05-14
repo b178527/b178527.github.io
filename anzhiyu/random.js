c456f8f43276e87ea8f7092dd4725f0d81b74694
b178527 <1157864699@qq.com>
2026-05-14 16:56:09 +0800

    Site updated: 2026-05-14 16:56:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["5ac32811adb9/","728d0100c79c/","1c4a5286cf6d/","102a0471e3ec/","651cb7fa30c0/"];function toRandomPost(){
+var posts=["728d0100c79c/","1c4a5286cf6d/","5ac32811adb9/","651cb7fa30c0/","102a0471e3ec/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
