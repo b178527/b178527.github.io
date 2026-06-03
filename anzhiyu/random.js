37572c8e4f8b33a18afdde214043eb74657e8173
b178527 <1157864699@qq.com>
2026-06-03 15:35:05 +0800

    Site updated: 2026-06-03 15:35:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["4b0dd9840b90/","728d0100c79c/","102a0471e3ec/","651cb7fa30c0/","5ac32811adb9/","1c4a5286cf6d/"];function toRandomPost(){
+var posts=["102a0471e3ec/","651cb7fa30c0/","1c4a5286cf6d/","5ac32811adb9/","728d0100c79c/","4b0dd9840b90/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
